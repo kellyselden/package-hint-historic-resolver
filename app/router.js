43332021f2dab67fceb094b31818a1ef3ecee622
Kelly Selden <kellyselden@gmail.com>
2015-11-12 21:34:58 -0500

    ember-cli@1.13.12

--- app/router.js
+++ app/router.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import config from './config/environment';
 import EmberMetricsRouterMixin from 'ember-metrics-mixins/mixins/router';
 
-var Router = Ember.Router.extend(EmberMetricsRouterMixin, {
+const Router = Ember.Router.extend(EmberMetricsRouterMixin, {
   location: config.locationType
 });
 
