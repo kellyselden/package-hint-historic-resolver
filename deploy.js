bc8d3a1bf81f7c02c2d5d12e4c2212d9b5267a59
Kelly Selden <kellyselden@gmail.com>
2015-11-16 09:49:25 -0500

    moving commands

--- deploy.js
+++ deploy.js
@@ -7,9 +7,9 @@ var commands = [
   'heroku config:set REBUILD_ALL=true',
   'heroku plugins:install https://github.com/heroku/heroku-repo.git',
   'heroku repo:purge_cache -a ' + appName,
+  'git push heroku master',
   'heroku config:unset REBUILD_ALL',
-  'heroku plugins:uninstall heroku-repo',
-  'git push heroku master'
+  'heroku plugins:uninstall heroku-repo'
 ];
 
 commands.forEach(function(command) {
