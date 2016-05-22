8b8e40f0425f5b8f8c2ac4d35c5e31fb79117a00
Kelly Selden <kellyselden@gmail.com>
2016-05-21 20:29:36 -0700

    finish dependency-date testing

--- tests/integration/components/dependency-date-test.js
+++ tests/integration/components/dependency-date-test.js
@@ -8,7 +8,7 @@ let server;
 let commitsCallback, packageCallback;
 let commitsRequest,  packageRequest;
 let getCommitsResponse, getPackageResponse;
-let date, repo, commit;
+let testDate, testRepo, testCommit;
 let onDateChanged, onFoundCommitData, onReceivedJson, onError;
 let commitRequestCount, packageRequestCount;
 
@@ -18,9 +18,9 @@ moduleForComponent('dependency-date', 'Integration | Component | dependency date
     server = new Pretender();
     server.prepareBody = JSON.stringify;
 
-    date = new Date(Date.UTC(2012, 2, 23, 4, 55, 24, 778));
-    repo = 'test-repo';
-    commit = '35345';
+    testDate = new Date(Date.UTC(2012, 2, 23, 4, 55, 24, 778));
+    testRepo = 'test-repo';
+    testCommit = '35345';
 
     onDateChanged = () => {};
     onFoundCommitData = () => {};
@@ -31,7 +31,7 @@ moduleForComponent('dependency-date', 'Integration | Component | dependency date
     packageCallback = () => {};
 
     getCommitsResponse = () => ([200, {}, [{
-      sha: commit
+      sha: testCommit
     }]]);
     getPackageResponse = () => ([200, {}, {
       contents: 'test-contents'
@@ -63,8 +63,8 @@ moduleForComponent('dependency-date', 'Integration | Component | dependency date
 });
 
 function render() {
-  this.set('date', date);
-  this.set('repo', repo);
+  this.set('date', testDate);
+  this.set('repo', testRepo);
 
   this.on('dateChanged', onDateChanged);
   this.on('foundCommitData', onFoundCommitData);
@@ -96,14 +96,8 @@ function waitForCalandar(context) {
   });
 }
 
-function waitForDateChange(context) {
-  return waitForCalandar(context).then(() => {
-    $('.xdsoft_date:not(.xdsoft_current):first').click();
-  });
-}
-
 test('renders', function(assert) {
-  assert.expect(1);
+  assert.expect(2);
 
   // date can be a positional param
   this.render(hbs`
@@ -115,6 +109,9 @@ test('renders', function(assert) {
   // shows input
   assert.strictEqual(this.$('.date-time-picker').length, 1);
 
+  // has unique class
+  assert.strictEqual(this.$('.dependency-date').length, 1);
+
   // calendar pops up
   return waitForCalandar(this);
 });
@@ -122,42 +119,38 @@ test('renders', function(assert) {
 test('can change date', function(assert) {
   assert.expect(1);
 
-  let done = assert.async();
-
   onDateChanged = d => {
-    assert.notStrictEqual(d, date);
-
-    done();
+    assert.notStrictEqual(d, testDate);
   };
 
   render.call(this);
 
-  waitForDateChange(this);
+  return waitForCalandar(this).then(() => {
+    $('.xdsoft_date:not(.xdsoft_current):first').click();
+  });
 });
 
 test('calls github api for commits', function(assert) {
   assert.expect(2);
 
-  let done = assert.async();
-
-  commitsCallback = ({ params, queryParams }) => {
-    assert.strictEqual(params.repo, repo);
-    assert.strictEqual(queryParams.until, '2012-03-23T04:55:24.778Z');
-
-    done();
+  commitsCallback = ({ params: { repo }, queryParams: { until } }) => {
+    assert.strictEqual(repo, testRepo);
+    assert.strictEqual(until, '2012-03-23T04:55:24.778Z');
   };
 
   render.call(this);
+
+  return wait();
 });
 
 test('calls github api again if repo changes', function(assert) {
   assert.expect(2);
 
-  commitsCallback = ({ params }) => {
-    if (params.repo === repo) {
+  commitsCallback = ({ params: { repo } }) => {
+    if (repo === testRepo) {
       this.set('repo', 'kjllkjasdlfkj');
     }
-    if (params.repo === 'kjllkjasdlfkj') {
+    if (repo === 'kjllkjasdlfkj') {
       assert.ok(true);
     }
   };
@@ -172,11 +165,11 @@ test('calls github api again if repo changes', function(assert) {
 test('calls github api again if date changes', function(assert) {
   assert.expect(2);
 
-  commitsCallback = ({ queryParams }) => {
-    if (queryParams.until === '2012-03-23T04:55:24.778Z') {
+  commitsCallback = ({ queryParams: { until } }) => {
+    if (until === '2012-03-23T04:55:24.778Z') {
       this.set('date', new Date(Date.UTC(2013, 2, 23, 4, 55, 24, 778)));
     }
-    if (queryParams.until === '2013-03-23T04:55:24.778Z') {
+    if (until === '2013-03-23T04:55:24.778Z') {
       assert.ok(true);
     }
   };
@@ -217,61 +210,62 @@ test('doesn\'t call github api again if date is removed', function(assert) {
 });
 
 test('handles error retrieving commits', function(assert) {
-  assert.expect(1);
+  assert.expect(2);
 
-  let done = assert.async();
+  let called = false;
+  onFoundCommitData = () => {
+    called = true;
+  };
 
   onError = error => {
     assert.strictEqual(error, 'Error retrieving latest commit: Error: Ajax operation failed');
-
-    done();
   };
 
   getCommitsResponse = () => [500, {}, {}];
 
   render.call(this);
+
+  return wait().then(() => {
+    assert.notOk(called);
+  });
 });
 
 test('sends latest commit data', function(assert) {
   assert.expect(1);
 
-  let done = assert.async();
-
   onFoundCommitData = latestCommit => {
     assert.deepEqual(latestCommit, {
-      sha: commit
+      sha: testCommit
     });
-
-    done();
   };
 
   render.call(this);
+
+  return wait();
 });
 
 test('calls github api for package.json', function(assert) {
   assert.expect(2);
 
-  let done = assert.async();
-
-  packageCallback = ({ params }) => {
-    assert.strictEqual(params.repo, repo);
-    assert.strictEqual(params.commit, commit);
-
-    done();
+  packageCallback = ({ params: { repo, commit } }) => {
+    assert.strictEqual(repo, testRepo);
+    assert.strictEqual(commit, testCommit);
   };
 
   render.call(this);
+
+  return wait();
 });
 
 test('gets new package.json if repo and commit changes', function(assert) {
   assert.expect(2);
 
-  packageCallback = ({ params }) => {
-    if (params.commit === commit) {
-      commit = '95670';
+  packageCallback = ({ params: { commit } }) => {
+    if (commit === testCommit) {
+      testCommit = '95670';
       this.set('repo', 'kjllkjasdlfkj');
     }
-    if (params.commit === '95670') {
+    if (commit === '95670') {
       assert.ok(true);
     }
   };
@@ -286,12 +280,12 @@ test('gets new package.json if repo and commit changes', function(assert) {
 test('gets new package.json if date and commit changes', function(assert) {
   assert.expect(2);
 
-  packageCallback = ({ params }) => {
-    if (params.commit === commit) {
-      commit = '95670';
+  packageCallback = ({ params: { commit } }) => {
+    if (commit === testCommit) {
+      testCommit = '95670';
       this.set('date', new Date(Date.UTC(2013, 2, 23, 4, 55, 24, 778)));
     }
-    if (params.commit === '95670') {
+    if (commit === '95670') {
       assert.ok(true);
     }
   };
@@ -307,7 +301,7 @@ test('doesn\'t get new package.json if repo becomes empty', function(assert) {
   assert.expect(1);
 
   packageCallback = () => {
-    commit = '95670';
+    testCommit = '95670';
     this.set('repo', null);
   };
 
@@ -322,8 +316,22 @@ test('doesn\'t get new package.json if date becomes empty', function(assert) {
   assert.expect(1);
 
   packageCallback = () => {
-    commit = '95670';
+    testCommit = '95670';
     this.set('date', null);
+  };
+
+  render.call(this);
+
+  return wait().then(() => {
+    assert.strictEqual(packageRequestCount, 1);
+  });
+});
+
+test('doesn\'t get new package.json if commit is same after date change', function(assert) {
+  assert.expect(1);
+
+  packageCallback = () => {
+    this.set('date', new Date(Date.UTC(2013, 2, 23, 4, 55, 24, 778)));
   };
 
   render.call(this);
@@ -336,31 +344,34 @@ test('doesn\'t get new package.json if date becomes empty', function(assert) {
 test('sends package.json contents', function(assert) {
   assert.expect(1);
 
-  let done = assert.async();
-
   onReceivedJson = data => {
     assert.deepEqual(data, {
       contents: 'test-contents'
     });
-
-    done();
   };
 
   render.call(this);
+
+  return wait();
 });
 
 test('handles error retrieving package.json', function(assert) {
-  assert.expect(1);
+  assert.expect(2);
 
-  let done = assert.async();
+  let called = false;
+  onReceivedJson = () => {
+    called = true;
+  };
 
   onError = error => {
     assert.strictEqual(error, 'Error retrieving package.json: Error: Ajax operation failed');
-
-    done();
   };
 
   getPackageResponse = () => [500, {}, {}];
 
   render.call(this);
+
+  return wait().then(() => {
+    assert.notOk(called);
+  });
 });
