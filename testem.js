f005a60a4868d3f23de80e5737bf2daed170f435
Kelly Selden <kellyselden@gmail.com>
2018-05-04 23:54:55 -0700

    use headless firefox

--- testem.js
+++ testem.js
@@ -2,11 +2,19 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
+  parallel: 5,
   launch_in_ci: [
     'Firefox'
   ],
   launch_in_dev: [
     'Chrome'
   ],
-  parallel: 5
+  browser_args: {
+    Firefox: {
+      mode: 'ci',
+      args: [
+        '-headless'
+      ]
+    }
+  }
 };
