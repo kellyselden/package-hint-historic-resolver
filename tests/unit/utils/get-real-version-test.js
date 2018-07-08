32cb63f371fb48fabbfaf87113ff0f09852dd7b6
Kelly Selden <kellyselden@gmail.com>
2018-07-08 17:16:17 +0100

    new unit test style

--- tests/unit/utils/get-real-version-test.js
+++ tests/unit/utils/get-real-version-test.js
@@ -8,34 +8,34 @@ const versions = [
   ['2.0.0', '5/1/2016']
 ];
 
-module('Unit | Utility | get real version');
+module('Unit | Utility | get real version', function() {
+  test('early exits', function(assert) {
+    let result = getRealVersion();
 
-test('early exits', function(assert) {
-  let result = getRealVersion();
+    assert.notOk(result);
+  });
 
-  assert.notOk(result);
-});
+  test('respects date ceiling', function(assert) {
+    let dateCeiling = new Date('2/1/2016');
 
-test('respects date ceiling', function(assert) {
-  let dateCeiling = new Date('2/1/2016');
+    let version = getRealVersion(versionHint, versions, dateCeiling);
 
-  let version = getRealVersion(versionHint, versions, dateCeiling);
+    assert.strictEqual(version, '1.0.1');
+  });
 
-  assert.strictEqual(version, '1.0.1');
-});
+  test('version equal to date ceiling is valid', function(assert) {
+    let dateCeiling = new Date('3/1/2016');
 
-test('version equal to date ceiling is valid', function(assert) {
-  let dateCeiling = new Date('3/1/2016');
+    let version = getRealVersion(versionHint, versions, dateCeiling);
 
-  let version = getRealVersion(versionHint, versions, dateCeiling);
+    assert.strictEqual(version, '1.1.0');
+  });
 
-  assert.strictEqual(version, '1.1.0');
-});
+  test('respects version hint', function(assert) {
+    let dateCeiling = new Date('6/1/2016');
 
-test('respects version hint', function(assert) {
-  let dateCeiling = new Date('6/1/2016');
+    let version = getRealVersion(versionHint, versions, dateCeiling);
 
-  let version = getRealVersion(versionHint, versions, dateCeiling);
-
-  assert.strictEqual(version, '1.1.0');
+    assert.strictEqual(version, '1.1.0');
+  });
 });
