1376a770cc1a78efec5d8dde69dfc663c6bde984
Kelly Selden <kellyselden@gmail.com>
2018-07-09 09:00:04 +0100

    more test updates

--- tests/integration/components/repo-date-row-test.js
+++ tests/integration/components/repo-date-row-test.js
@@ -1,20 +1,23 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import wait from 'ember-test-helpers/wait';
 import sinon from 'sinon';
 import testDateChange from '../../helpers/test-date-change';
+import { setupRender } from 'ember-test-setup';
 
-let title;
-let repoDate;
-let repoDateError;
-let commit;
-let commitDate;
-let commitError;
-let changeRepoDate;
+module('Integration | Component | repo date row', function(hooks) {
+  setupRenderingTest(hooks);
 
-moduleForComponent('repo-date-row', 'Integration | Component | repo date row', {
-  integration: true,
-  beforeEach() {
+  let title;
+  let repoDate;
+  let repoDateError;
+  let commit;
+  let commitDate;
+  let commitError;
+  let changeRepoDate;
+
+  hooks.beforeEach(function() {
     title = 'test title';
     repoDate = new Date(2015, 1, 2);
     repoDateError = undefined;
@@ -23,73 +26,70 @@ moduleForComponent('repo-date-row', 'Integration | Component | repo date row', {
     commitError = undefined;
 
     changeRepoDate = sinon.spy();
-  }
-});
-
-function render() {
-  this.set('title', title);
-  this.set('repoDate', repoDate);
-  this.set('repoDateError', repoDateError);
-  this.set('commit', commit);
-  this.set('commitDate', commitDate);
-  this.set('commitError', commitError);
-
-  this.on('changeRepoDate', changeRepoDate);
-
-  this.render(hbs`
-    {{repo-date-row
-      title=title
-      repoDate=repoDate
-      repoDateError=repoDateError
-      commit=commit
-      commitDate=commitDate
-      commitError=commitError
-      changeRepoDate=(action "changeRepoDate")
-    }}
-  `);
-}
-
-test('shows defaults', function(assert) {
-  assert.expect(3);
-
-  render.call(this);
-
-  assert.strictEqual(this.$('.title').text().trim(), title);
-  assert.strictEqual(this.$('.date-time-picker').val(), '2015/02/02 0:00');
-  assert.strictEqual(this.$('.commit').text().trim(), `Latest commit ${commit} on ${commitDate}`);
-});
-
-test('shows commit error', function(assert) {
-  assert.expect(2);
-
-  commitError = 'test commit error';
-
-  render.call(this);
-
-  return wait().then(() => {
-    assert.strictEqual(this.$('.commit-error').text().trim(), `Error getting commit: ${commitError}`);
-    assert.strictEqual(this.$('.commit').length, 0);
   });
-});
 
-test('shows repo working date error', function(assert) {
-  assert.expect(1);
+  let render = setupRender(hooks, {
+    beforeRender() {
+      this.set('title', title);
+      this.set('repoDate', repoDate);
+      this.set('repoDateError', repoDateError);
+      this.set('commit', commit);
+      this.set('commitDate', commitDate);
+      this.set('commitError', commitError);
 
-  repoDateError = 'test repo date error';
-
-  render.call(this);
-
-  return wait().then(() => {
-    assert.strictEqual(this.$('.repo-date-error').text().trim(), `Error getting package.json: ${repoDateError}`);
+      this.set('changeRepoDate', changeRepoDate);
+    },
+    template: hbs`
+      {{repo-date-row
+        title=title
+        repoDate=repoDate
+        repoDateError=repoDateError
+        commit=commit
+        commitDate=commitDate
+        commitError=commitError
+        changeRepoDate=(action changeRepoDate)
+      }}
+    `
   });
-});
 
-test('sends date update event', function(assert) {
-  return testDateChange(
-    assert,
-    render.bind(this),
-    '.date-time-picker',
-    repoDate,
-    changeRepoDate
-  );
+  test('shows defaults', async function(assert) {
+    assert.expect(3);
+
+    await render();
+
+    assert.strictEqual(find('.title').textContent.trim(), title);
+    assert.strictEqual(find('.date-time-picker').value, '2015/02/02 0:00');
+    assert.strictEqual(find('.commit').textContent.trim(), `Latest commit ${commit} on ${commitDate}`);
+  });
+
+  test('shows commit error', async function(assert) {
+    assert.expect(2);
+
+    commitError = 'test commit error';
+
+    await render();
+
+    assert.strictEqual(find('.commit-error').textContent.trim(), `Error getting commit: ${commitError}`);
+    assert.notOk(find('.commit'));
+  });
+
+  test('shows repo working date error', async function(assert) {
+    assert.expect(1);
+
+    repoDateError = 'test repo date error';
+
+    await render();
+
+    assert.strictEqual(find('.repo-date-error').textContent.trim(), `Error getting package.json: ${repoDateError}`);
+  });
+
+  test('sends date update event', function(assert) {
+    return testDateChange(
+      assert,
+      render,
+      '.date-time-picker',
+      repoDate,
+      changeRepoDate
+    );
+  });
 });
